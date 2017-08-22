194a60e8ae440eac732c803d7d5806a3e4ca1fc3
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-22 13:54:54 -0700

    day 2 robot square

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -5,10 +5,30 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
+		// START HERE
+		Robot JohnWalsh = new Robot();
+		JohnWalsh.penDown();
+		JohnWalsh.setSpeed(10);
+
+		for (int i = 0; i < 4; i++) {
+			JohnWalsh.move(50);
+			JohnWalsh.turn(90);
+
+		}
+		JohnWalsh.penUp();
+		JohnWalsh.move(120);
+		JohnWalsh.turn(90);
+		for (int i = 0; i < 4; i++) {
+			JohnWalsh.penDown();
+			JohnWalsh.move(50);
+			JohnWalsh.turn(90);
+
+		}JohnWalsh.turn(180);
+		JohnWalsh.penUp();
+		JohnWalsh.move(120);
+		JohnWalsh.setAngle(90);
+		
 		
 		
 	}
